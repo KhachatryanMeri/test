03356927e014e75a46e6570930066512541b3b19
Meri Khachatryan <KhachatryanMeri@outlook.com>
2026-05-03 19:24:33 +0400

    Initial Commit

---
+++ TestFastlaneGithub/TestFastlaneGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFastlaneGithubApp.swift
+//  TestFastlaneGithub
+//
+//  Created by Meri Khachatryan on 03.05.26.
+//
+
+import SwiftUI
+
+@main
+struct TestFastlaneGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestFastlaneGithubTests/TestFastlaneGithubTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestFastlaneGithubTests.swift
+//  TestFastlaneGithubTests
+//
+//  Created by Meri Khachatryan on 03.05.26.
+//
+
+import Testing
+@testable import TestFastlaneGithub
+
+struct TestFastlaneGithubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
